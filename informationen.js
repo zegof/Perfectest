093952eedf0ee8b8d4c8a87d22475eee00be9c36
zegof <117827585+zegof@users.noreply.github.com>
2024-12-18 07:44:05 +0100

    Update informationen.js

--- informationen.js
+++ informationen.js
@@ -1,71 +1,35 @@
-const infoDateipfad = 'informationen.txt';
-
-// Asynchronen Dateilesevorgang starten
-var informationen;
+const infoDateipfad = 'informationen.txt'; 
+var informationen; 
 var wörterImSatz;
-fetch (infoDateipfad)
-    .then(response => response.text())
-    .then(daten => {
-        daten = daten.toLowerCase();
-        informationen = daten.split('\n');
-        console.log(informationen);
 
+fetch(infoDateipfad)
+  .then(response => response.text())
+  .then(daten => {
+    daten = daten.toLowerCase();
+    informationen = daten.split('\n');
+    console.log(informationen);
+  })
+  .catch(err => {
+    console.error('Fehler beim Lesen der Datei:', err);
+  });
 
-    })
-    .catch(err => {
-        console.error('Fehler beim Lesen der Datei:', err);
-    });
-
-
-
-
- 
-
-
-// Funktion zum Finden passender Sätze
 function findePassendeInformationen(eingabeWörter) {
+  let besteÜbereinstimmung = { satz: '', anzahl: 0 };
 
-    // Initialisieren einer leeren Liste für passende Sätze
-    const passendeSätze = [];
-    console.log('informationen:' +informationen)
-    console.log('passendeSätze:' +passendeSätze)
-    // Iterieren über jeden Satz
-    for (const satz of informationen) {
+  for (const satz of informationen) {
+    const wörterImSatz = satz.split(" ");
+    const anzahl = eingabeWörter.filter(wort => wörterImSatz.includes(wort)).length;
 
-        const wörterImSatz = satz.split(" ");
-        console.log('Wörterimsatz' +wörterImSatz)
-        console.log(satz)
-        // Zählen der Anzahl der Eingabewörter im Satz
-        const anzahl = eingabeWörter.filter(wort => wörterImSatz.includes(wort)).length;
-        console.log('anzahl:' +anzahl);
-        console.log(satz)
-        // Wenn mindestens zwei Eingabewörter gefunden wurden, fügen Sie den Satz zu den passenden Sätzen hinzu
-        if (anzahl >= 2) {
-            passendeSätze.push(satz);
-        }
-        console.log(satz)
+    if (anzahl > besteÜbereinstimmung.anzahl) {
+      besteÜbereinstimmung = { satz, anzahl };
     }
+  }
 
-        
-        const passendeErgebnisse = passendeSätze;
-        console.log(passendeErgebnisse);
-        
-        
-       
-        if (passendeErgebnisse.length > 0) {
-            console.log("Passende Informationen:");
-            for (const satz of passendeErgebnisse) {
-                console.log("-", satz);
-                sendMes(satz);
-            }
-        } else {
-            console.log("Keine passenden Daten gefunden.");
-            sendMes('Entschuldigung, ich verstehe nicht ganz. Kannst du das genauer oder mit anderen Worten erklären?');
-            if (lernen == 'on') {
-                
-
-            }
-        }
-
+  if (besteÜbereinstimmung.anzahl > 0) {
+    console.log("Beste Übereinstimmung:", besteÜbereinstimmung.satz);
+    sendMes(besteÜbereinstimmung.satz);
+  } else {
+    console.log("Keine passenden Daten gefunden.");
+    sendMes('Entschuldigung, ich verstehe nicht ganz. Kannst du das genauer oder mit anderen Worten erklären?');
+  }
 }
-
